4ce220837f56576a7dbbfefc3aabd15893f3985a
sherri92123123 <100654342+sherri92123123@users.noreply.github.com>
2022-03-01 10:46:53 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    target += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("" + (target))
+})
+input.onButtonPressed(Button.B, function () {
+    target += -1
+})
+let target = 0
+basic.showIcon(IconNames.Heart)
+target = 50
 basic.forever(function () {
 	
 })
